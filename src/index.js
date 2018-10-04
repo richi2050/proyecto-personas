ac1f30d7c9d38840951fc90779d27a5124de1762
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-10-04 13:44:51 -0500

    configuracion de redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//se manda a llamar componentes de redux, createStore: crea una mega store, applyMiddleware: utiliza para poner todos los middleware posibles antes de reduxes
+import  { createStore, applyMiddleware } from 'redux';
+//encargado de conectar react con redux
+import { Provider } from 'react-redux';
+// instalado para tener un control sobre las rutas
+import  createHistory  from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createHistory();
+const middleware = [routerMiddleware(history), thunk];
+const store = createStore(
+  reducers,
+  applyMiddleware(...middleware)
+);
+
+ReactDOM.render(
+    <Provider store= { store }>
+      <App />
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
