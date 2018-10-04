a3a064e006bd7b9bef3630ae43d0cbd55b6b1e1e
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-10-04 14:17:55 -0500

    configuracion de Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Router } from 'react-router';
+import PropTypes from 'prop-types';
 import './App.css';
+
+import Home  from './views/home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router history={ this.props.history }>
+        <div className="App">
+          <Route exact path="/" component={ Home }/>
+        </div>
+      </Router>
     );
   }
+}
+App.PropTypes = {
+  history: PropTypes.any.isRequired
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store= { store }>
-      <App />
+      <App history={ history }/>
     </Provider>,
     document.getElementById('root'));
 
