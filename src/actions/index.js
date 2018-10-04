7394d8e9a02a72286735cf5431d1d93c662cc038
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-10-04 14:44:33 -0500

    Configuracion de redux

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import http from './http'
+
+//getUsers
+
+export const getUsers = () => {
+  return ( dispatch, getState ) => {
+    // Todo el request action
+    console.log('se llama accion getUsers');
+  }
+}
